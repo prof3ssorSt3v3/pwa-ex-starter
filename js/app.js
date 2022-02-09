2e77c8f23d1e0fa50799e14abbc71c15d660eb33
Prof3ssorSt3v3 <griffis@algonquincollege.com>
2022-02-09 12:05:31 -0500

    updated the repo code to expand the starter code

--- js/app.js
+++ js/app.js
@@ -1,18 +1,70 @@
 const APP = {
   sw: null,
-  isOnline: true,
+  deferredPrompt: null, //used for installing later
+  isOnline: true, //TODO: check navigator.onLine
   init: () => {
-    //when the page loads
+    //when the page loads (runs on every page load)
+    APP.registerSW();
+    APP.addListeners();
+    APP.updateNavCount();
   },
   registerSW: () => {
     //register the service worker
-    //add listener for message
+    navigator.serviceWorker.register('/sw.js').catch(function (err) {
+      // Something went wrong during registration. The sw.js file
+      // might be unavailable or contain a syntax error.
+      console.warn(err);
+    });
+    navigator.serviceWorker.ready.then((registration) => {
+      // .ready will never reject... just wait indefinitely
+      APP.sw = registration.active;
+      //save the reference to use later or use .ready again
+    });
   },
-  installSW: () => {
-    //install the service worker
+  addListeners: () => {
+    //add event listeners for DOM
+    //TODO: add dom listener for navigating between home and other.
+
+    //add event listeners for online and offline
+    window.addEventListener('online', APP.changeStatus);
+    window.addEventListener('offline', APP.changeStatus);
+
+    //add listener for message
+    navigator.serviceWorker.addEventListener('message', APP.gotMessage);
+
+    //add listener for install event
+    window.addEventListener('beforeinstallprompt', (ev) => {
+      // Prevent the mini-infobar from appearing on mobile
+      ev.preventDefault();
+      // Save the event in a global property
+      // so that it can be triggered later.
+      APP.deferredPrompt = ev;
+      console.log('deferredPrompt saved');
+      // Build your own enhanced install experience
+      // use the APP.deferredPrompt saved event
+    });
+  },
+  changeStatus: (ev) => {
+    //toggling between online and offline
+    APP.isOnline = ev.type === 'online' ? true : false;
+    //TODO: send message to sw about being online or offline
+    //TODO: update something in the HTML to show offline
+  },
+  gotMessage: (ev) => {
+    //received message from service worker
+    console.log(ev.data);
   },
   sendMessage: (msg) => {
     //send messages to the service worker
+    navigator.serviceWorker.ready.then((registration) => {
+      registration.active.postMessage(msg);
+    });
+  },
+  updateNavCount: (ev) => {
+    //TODO: when the app loads check to see if the sessionStorage key
+    //exists and if the number is greater than 4.
+    //if yes, then call the deferred install prompt
+    //check for null on APP.deferredPrompt
   },
 };
 
--- sw.js
+++ sw.js
@@ -1,17 +1,111 @@
 const version = 1;
-const cacheName = 'pwaExerciseCache';
+let isOnline = true; //will get updated via messaging
+const staticCache = `pwaEx3StaticCache${version}`;
+const dynamicCache = `pwaEx3DynamicCache${version}`;
 const cacheList = [
   '/',
   '/index.html',
+  '/other.html',
+  '/404.html',
   '/css/main.css',
   '/js/app.js',
-  '/404.html',
   '/manifest.json',
+  //TODO: add all the icons from the img folder
+  //TODO: add a google font in your css and here
+  //TODO: if you add any other JS files include them here
 ];
-//TODO: add a google font in your css and here
 
 //TODO: complete the functions for these events
-self.addEventListener('install', () => {});
-self.addEventListener('activate', () => {});
-self.addEventListener('fetch', () => {});
-self.addEventListener('message', () => {});
+self.addEventListener('install', (ev) => {
+  ev.waitUntil(
+    caches.open(cacheName).then((cache) => {
+      //save the whole cacheList
+      cache.addAll(cacheList);
+    })
+  );
+});
+
+self.addEventListener('activate', (ev) => {
+  ev.waitUntil(
+    caches
+      .keys()
+      .then((keys) => {
+        return Promise.all(
+          keys
+            .filter((key) => {
+              if (key === staticCache || key === dynamicCache) {
+                return false;
+              } else {
+                return true;
+              }
+            })
+            .map((key) => caches.delete(key))
+        ); //keys.filter().map() returns an array of Promises
+      })
+      .catch(console.warn)
+  );
+});
+
+self.addEventListener('fetch', (ev) => {
+  //TODO:
+  //update the script here to look in the caches first
+  //if not in the cache check if online
+  //if online, do a fetch
+  //if fetch returns 404 and request.mode == 'navigation'
+  //return the 404 page from the cache
+  //save any new fetched files in the dynamic cache
+  ev.respondWith(
+    caches.match(ev.request).then((cacheRes) => {
+      return (
+        cacheRes ||
+        fetch(ev.request)
+          .then((fetchRes) => {
+            //TODO: check here for the 404 error
+            
+            return caches.open(dynamicCache).then((cache) => {
+              let copy = fetchRes.clone(); //make a copy of the response
+              cache.put(ev.request, copy); //put the copy into the cache
+              return fetchRes; //send the original response back up the chain
+            });
+          })
+          .catch((err) => {
+            console.log('SW fetch failed');
+            console.warn(err);
+            //if we were offline then the fetch() will fail
+            //check for 404 and ev.request properties
+            //ev.request.mode == 'navigation'
+            //ev.request.url.pathname
+            //ev.request.headers.get('Content-Type');
+          })
+      );
+    })
+  ); //what do we want to send to the browser?
+};);
+
+self.addEventListener('message', (ev) => {
+  console.log(ev.data);
+  //message received from script
+  if (ev.data.ONLINE) {
+    isOnline = ev.data.ONLINE;
+    //we could confirm if actually online and send a message to the browser if not
+    // use a fetch with method: HEAD to do this
+    // in the webpage-side code set a timer to resend the online message 
+    // which will trigger this code again
+  }
+  //handle other messages from the browser...
+  //EG: CLEARDYNAMICCACHE, CLEARSTATICCACHE, LOADFILE, CONFIRMONLINE, 
+  //    GETFROMDB, etc
+});
+
+function sendMessage(msg) {
+  //send a message to the browser
+  //from the service Worker
+  //code from messaging.js Client API send message code
+  self.clients.matchAll().then(function (clients) {
+    if (clients && clients.length) {
+      //Respond to last focused tab
+      clients[0].postMessage(msg);
+    }
+  });
+  //See the code from the online video for the version that messages ALL Clients
+}
